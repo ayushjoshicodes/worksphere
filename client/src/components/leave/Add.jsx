8fa36681a103e8ca0319d44905e87e508f9c7dbf
ayushjoshicodes <ayushjoshi14032005@gmail.com>
2025-06-25 22:40:33 +0530

    Update Add.jsx

--- client/src/components/leave/Add.jsx
+++ client/src/components/leave/Add.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/authContext.jsx';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
